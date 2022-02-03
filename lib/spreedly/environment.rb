72de8d9fc1029600a921e54a640ad765bece4fa6
Vince Puzzella <vincepuz@gmail.com>
2022-02-03 20:40:55 +0000

    Add 'allow_expired_date' and 'allow_blank_date' to update_credit_card_body options

--- lib/spreedly/environment.rb
+++ lib/spreedly/environment.rb
@@ -278,7 +278,7 @@ module Spreedly
       build_xml_request('payment_method') do |doc|
         add_to_doc(doc, options, :email, :month, :full_name, :first_name, :last_name, :year,
                    :address1, :address2, :city, :state, :zip, :country, :phone_number,
-                   :eligible_for_card_updater, :allow_blank_name)
+                   :eligible_for_card_updater, :allow_blank_name, :allow_expired_date, :allow_blank_date)
       end
     end
 
